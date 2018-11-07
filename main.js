88feb356c20d77493acaa25f289448fed2a9d36d
Jacey Davis <jaceyd923@gmail.com>
2018-11-06 20:28:20 -0700

    added potter cards to DOM with initial styling

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 import { characters } from '/potter.js'
+
+let cardContainer = document.querySelector('#card-container')
+
+characters.forEach(character => {
+    let fig = document.createElement('figure')
+    let img = document.createElement('img')
+    let cap = document.createElement('figcaption')
+    
+    img.src = character.image
+    cap.textContent = character.name
+    
+    cardContainer.appendChild(fig)
+    fig.appendChild(img)
+    fig.appendChild(cap)
+})
