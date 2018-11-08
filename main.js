cb7cfb1384925b2bbdf1353189c76b41beff6b95
Jacey Davis <jaceyd923@gmail.com>
2018-11-08 10:43:41 -0700

    added card flipping functionality

--- main.js
+++ main.js
@@ -3,14 +3,51 @@ import { characters } from '/potter.js'
 let cardContainer = document.querySelector('#card-container')
 
 characters.forEach(character => {
+    let scene = document.createElement('div')
+    scene.className = "scene"
+    cardContainer.appendChild(scene)
+
+    let card = document.createElement('div')
+    card.className = "card"
+    scene.appendChild(card)
+
+    //Creates front of card
     let fig = document.createElement('figure')
+    fig.classList.add('card-face')
+    fig.classList.add('front')
+    card.appendChild(fig)
+
     let img = document.createElement('img')
-    let cap = document.createElement('figcaption')
-    
     img.src = character.image
+    let cap = document.createElement('figcaption')
     cap.textContent = character.name
-    
-    cardContainer.appendChild(fig)
     fig.appendChild(img)
     fig.appendChild(cap)
+
+    //Creates back of card
+    let backDiv = document.createElement('div')
+    backDiv.classList.add('card-face')
+    backDiv.classList.add('back')
+    card.appendChild(backDiv)
+
+    let backNameHeading = document.createElement('h2')
+    backNameHeading.className = "back-name-heading"
+    backNameHeading.textContent = character.name
+    backDiv.appendChild(backNameHeading)
+
+    let propList = document.createElement('div')
+    propList.className = "prop-list"
+    backDiv.appendChild(propList)
+
+    //below is a placeholder
+    let ul = document.createElement('ul')
+    propList.appendChild(ul)
+
+    let li = document.createElement('li')
+    ul.appendChild(li)
+    //above is a placeholder
+
+    card.addEventListener( 'click', function() {
+        card.classList.toggle('is-flipped');
+    });
 })
