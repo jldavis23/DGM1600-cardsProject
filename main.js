f7cf5d8faa9e52aa012313d52d3a764678ee2888
Jacey Davis <jaceyd923@gmail.com>
2018-11-08 20:48:51 -0700

    added sort by house and by student/staff functionality and card colors

--- main.js
+++ main.js
@@ -3,7 +3,21 @@ import { characters } from '/potter.js'
 let cardContainer = document.querySelector('#card-container')
 let wantedProps = ['house', "dateOfBirth", "ancestry", "patronus", "actor", "alive"]
 
-characters.forEach(character => {
+//BUTTONS
+let noneButton = document.querySelector("#none-button")
+let HouseButton = document.querySelector("#house-button")
+let studentsStaffButton = document.querySelector("#students-staff-button")
+let aliveDeadButton = document.querySelector("#alive-dead-button")
+
+//MAIN FUNCTIONS --------------------------------------------------------------
+let removeCards = () => {
+    let removeDiv = document.getElementById("card-container");
+    while (removeDiv.firstChild) {
+        removeDiv.removeChild(removeDiv.firstChild);
+    }
+}
+
+let createCards = (character) => {
     let scene = document.createElement('div')
     scene.className = "scene"
     cardContainer.appendChild(scene)
@@ -14,8 +28,7 @@ characters.forEach(character => {
 
     //Creates front of card
     let fig = document.createElement('figure')
-    fig.classList.add('card-face')
-    fig.classList.add('front')
+    fig.classList.add('card-face', 'front')
     card.appendChild(fig)
 
     let img = document.createElement('img')
@@ -27,8 +40,10 @@ characters.forEach(character => {
 
     //Creates back of card
     let backDiv = document.createElement('div')
-    backDiv.classList.add('card-face')
-    backDiv.classList.add('back')
+    backDiv.classList.add('card-face', 'back')
+    if (character.house !== "") {
+        backDiv.classList.add(`${character.house}`)
+    }
     card.appendChild(backDiv)
 
     let backNameHeading = document.createElement('h2')
@@ -50,7 +65,6 @@ characters.forEach(character => {
             ul.appendChild(propKey)
             let propValue = document.createElement('li')
             propValue.textContent = character[prop]
-            // console.log(`${prop.charAt(0).toUpperCase() + prop.slice(1)}:`)
             ul.appendChild(propValue)
         }
     }
@@ -58,4 +72,65 @@ characters.forEach(character => {
     card.addEventListener( 'click', function() {
         card.classList.toggle('is-flipped');
     });
-})
+}
+
+let allCharacters = () => {
+    removeCards();
+    characters.forEach(character => {
+        createCards(character);
+    })
+}
+
+allCharacters();
+noneButton.addEventListener("click", allCharacters)
+
+
+//FILTER FUNCTIONS --------------------------------------------------------------
+
+//Sort by House
+let houses = ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"]
+
+let sortByHouse = () => {
+    removeCards()
+    houses.forEach(house => {
+        let filteredByHouse = characters.filter(character => character.house === house)
+        
+        let houseName = document.createElement('h2')
+        houseName.textContent = house
+        houseName.classList.add("house-heading", `${house}`)
+        cardContainer.appendChild(houseName)
+
+        filteredByHouse.forEach(character => {
+            createCards(character)
+        })
+    })
+}
+
+HouseButton.addEventListener("click", sortByHouse)
+
+//Sort by Students and Staff 
+let sortByStudentStaff = () => {
+    removeCards()
+    let students = characters.filter(character => character.hogwartsStudent === true)
+
+    let studentHeading = document.createElement('h2')
+    studentHeading.textContent = "Students"
+    cardContainer.appendChild(studentHeading)
+
+    students.forEach(student => {
+        createCards(student)
+    })
+
+    let staff = characters.filter(character => character.hogwartsStaff === true)
+
+    let staffHeading = document.createElement('h2')
+    staffHeading.textContent = "Staff"
+    cardContainer.appendChild(staffHeading)
+
+    staff.forEach(person => {
+        createCards(person)
+    })
+
+}
+
+studentsStaffButton.addEventListener("click", sortByStudentStaff)
